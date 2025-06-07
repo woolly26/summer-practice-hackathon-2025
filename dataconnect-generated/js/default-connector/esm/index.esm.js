d6db67029610db746f4063d6aa0cabed4acadaef
Mario Turtoi <84230374+woolly26@users.noreply.github.com>
2025-06-07 06:39:44 +0300

    Add files via upload

---
+++ dataconnect-generated/js/default-connector/esm/index.esm.js
@@ -0,0 +1,7 @@
+
+export const connectorConfig = {
+  connector: 'default',
+  service: 'gitgud1',
+  location: 'us-central1'
+};
+
---
+++ dataconnect-generated/js/default-connector/index.cjs.js
@@ -0,0 +1,7 @@
+
+const connectorConfig = {
+  connector: 'default',
+  service: 'gitgud1',
+  location: 'us-central1'
+};
+exports.connectorConfig = connectorConfig;
